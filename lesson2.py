1542c41af22a8cd8f0f2619264e1bd964ef90978
dachi <125451693+d-jey66@users.noreply.github.com>
2023-12-13 15:22:58 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+
+name=("dachi")
+
+surname=("jananashvili")
+
+age=("12")
+
+height=("156.5")
+
+knows_proggramming=False
+
+is_ugly=False
+
+print("name:" + name+ "  " + "surname:" + surname + "  " + "age:"+ age+ "  " + "height:" + height + "  " + "knows proggramming:" + "False" + "  " + "is ugly:" + "False")
