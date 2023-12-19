7069daec11aa90b540b2ea578a7b741b15532a45
dachi <125451693+d-jey66@users.noreply.github.com>
2023-12-19 16:39:01 +0400

    lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,14 +1,14 @@
 
-name=("dachi")
+name = "dachi"
 
-surname=("jananashvili")
+surname = "jananashvili"
 
-age=("12")
+age = 12
 
-height=("156.5")
+height = 156.5
 
-knows_proggramming=False
+knows_proggramming = False
 
-is_ugly=False
+is_ugly = False
 
-print("name:" + name+ "  " + "surname:" + surname + "  " + "age:"+ age+ "  " + "height:" + height + "  " + "knows proggramming:" + "False" + "  " + "is ugly:" + "False")
+print("name:" + name+ "  " + "surname:" + surname + "  " + "age:"+ str(age)+ "  " + "height:" + str(height) + "  " + "knows proggramming:" + "False" + "  " + "is ugly:" + "False")
